03d79f23f703e07144f6f7b13b9a04cc7e3c70a1
Payne Richards <paynerichards@Paynes-MacBook-Pro.local>
2017-07-10 10:24:53 -0500

    heroku ready

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var connectionString = 'mongodb://localhost/barHopperTest';
+var connectionString = process.env.DB_HOST;
 
 mongoose.connect(connectionString);
 
